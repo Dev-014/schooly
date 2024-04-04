48c6aa7834407599bdd3f54f6ee752a879f42416
Deepanshu Sondkar <deepanshu@Deepanshus-MacBook-Pro.local>
2024-04-04 11:51:26 +0530

    Add report card, announcment, timetable,homework, study material, leave page, fee detail page and many file and all the neccesary changes to run the app

---
+++ lib/screens/students_ui_2.0/feeDetil.dart
@@ -0,0 +1,84 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../../common_widgets/fee_detail_card.dart';
+
+class FeeD extends StatefulWidget {
+  const FeeD({super.key});
+
+  @override
+  State<FeeD> createState() => _FeeDState();
+}
+
+class _FeeDState extends State<FeeD> {
+
+  Future<Map<String, Map<String,dynamic>>?> func()async{
+    var a =  await FirebaseFirestore.instance.collection('/NewSchool/G0ITybqOBfCa9vownMXU/attendence/y2Yes9Dv5shcWQl9N9r2/fee_details').doc("859949").get();
+
+    Map<String, Map<String, dynamic>> convertedMap = {};
+
+    a.data()!.forEach((key, value) {
+      if (value is Map<String, dynamic>) {
+        convertedMap[key] = value;
+      }
+    });
+
+    print("MMMMMM");
+    print(a.data());
+
+    return convertedMap as Map<String, Map<String,dynamic>>?;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: FutureBuilder(
+
+        future: func(),
+         builder: (context,snapshot){
+           Map<String, Map<String,dynamic>>? mapOfMaps = snapshot.data;
+           if (snapshot.hasError) {
+             return Text('Error: ${snapshot.error}');
+           }
+
+           if (snapshot.connectionState == ConnectionState.waiting) {
+             return CircularProgressIndicator();
+           }
+           if (snapshot.hasData) {
+            print(snapshot.data);
+           }
+
+          return SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: snapshot.data!.keys.map((outerKey) {
+                final innerMap = mapOfMaps![outerKey];
+                if (innerMap != null) {
+                  var paymentDate = innerMap["payment date"]??"-";
+                  var paymentAmount = innerMap["payment amount"]??"-";
+                  var dueDate = innerMap["due date"]??"-";
+                  var status = innerMap["status"]?? "-";
+                  return Column(
+                    children: innerMap.keys.map((innerKey){
+                      return  FeeDetailCard(title: "title", paymentDate: paymentDate, paymentAmount: paymentAmount, dueDate: dueDate, status: status,);
+
+                    }).toList()
+                  );
+                  //   Column(
+                  //   children: innerMap.keys.map((innerKey) {
+                  //     final value = innerMap[innerKey];
+                  //     return ListTile(
+                  //       title: SelectionArea(child: Text('$outerKey - $innerKey: $value')),
+                  //     );
+                  //   }).toList(),
+                  // );
+                } else {
+                  return SizedBox(); // Handle missing inner map
+                }
+              }).toList(),
+            ),
+          );
+         },
+      ),
+    );
+  }
+}
